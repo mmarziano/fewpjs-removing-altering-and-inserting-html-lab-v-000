98d3514b7186c23afe66fb3d09af814d9624a8f6
Monica Marziano <mmarziano1@gmail.com>
2019-10-01 22:04:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ document.querySelector('main#main').remove();
 let div = document.createElement('div');
 
 
-var newHeader = document.querySelector('h1#victory');
+let newHeader = document.querySelector('h1#victory');
 
