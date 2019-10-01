c550ad289ffe230cade2747a5351917a484cb693
Monica Marziano <mmarziano1@gmail.com>
2019-10-01 21:59:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 document.querySelector('main#main').remove();
 
 let div = document.createElement('div');
-let h1 = document.createElement('h1#victory');
+let h1 = document.createElement('h1');
+
+h1.className = 'victory'
 
 div.appendChild(hi);
 
