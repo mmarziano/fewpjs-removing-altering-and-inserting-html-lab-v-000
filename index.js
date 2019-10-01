af3d1838f68395d869d628ea108b11f0df0b082c
Monica Marziano <mmarziano1@gmail.com>
2019-10-01 22:01:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,6 @@
 document.querySelector('main#main').remove();
 
 let div = document.createElement('div');
-let h1 = document.createElement('h1');
 
-h1.className = 'victory'
-
-div.appendChild(hi);
 
 var newHeader = document.querySelector('h1#victory');
